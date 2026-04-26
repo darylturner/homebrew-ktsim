726f23d94acffc7636fe7265c9c597f1297bf713
Daryl Turner <daryl.turner@protonmail.ch>
2026-04-26 12:12:40 +0100

    fix placeholders

--- Formula/ktsim.rb
+++ Formula/ktsim.rb
@@ -6,11 +6,11 @@ class Ktsim < Formula
   on_macos do
     on_arm do
       url "https://github.com/darylturner/ktsim/releases/download/v#{version}/ktsim-aarch64-macos"
-      sha256 "..."
+      sha256 "166bbbb13593374eb23a36f26e5926d043e272f8d5a81aa7c0552d249031b2e4"
     end
     on_intel do
       url "https://github.com/darylturner/ktsim/releases/download/v#{version}/ktsim-x86_64-macos"
-      sha256 "..."
+      sha256 "sha256:38b2a6f8be384058f9fe568eb0a389a36a55c1b16b20b84f8404f413214f788e"
     end
   end
 
