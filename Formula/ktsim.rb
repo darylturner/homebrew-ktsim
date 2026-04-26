0e2c044eff2973f1e0ae9e894212dc3e6a38a39c
Daryl Turner <daryl.turner@protonmail.ch>
2026-04-26 12:08:54 +0100

    first try

---
+++ Formula/ktsim.rb
@@ -0,0 +1,20 @@
+class Ktsim < Formula
+  desc "..."
+  homepage "https://github.com/darylturner/ktsim"
+  version "v0.3.5"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/darylturner/ktsim/releases/download/v#{version}/ktsim-aarch64-macos"
+      sha256 "..."
+    end
+    on_intel do
+      url "https://github.com/darylturner/ktsim/releases/download/v#{version}/ktsim-x86_64-macos"
+      sha256 "..."
+    end
+  end
+
+  def install
+    bin.install Dir["ktsim-*"].first => "ktsim"
+  end
+end
